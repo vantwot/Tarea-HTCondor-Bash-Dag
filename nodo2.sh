ecad63e48cd00d7a60fbefe47fdbee20870797a4
Van <45314803+vantwot@users.noreply.github.com>
2022-08-03 14:30:26 -0500

    Nodo 2

---
+++ nodo2.sh
@@ -0,0 +1,7 @@
+dato=$1
+registros=$(wc -l $dato)
+imcompletos=$(grep -c ", ?," $dato)
+sed -i "s/, ?,/,,/g" $dato 
+
+echo "Numero total de registros ${registros} y Numero de registos incompletos ${imcompletos}"
+
