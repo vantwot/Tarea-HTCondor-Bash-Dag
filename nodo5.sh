246e25d48aa4bab04d88349d6532f1d11a81d704
SISTEMAS <sistemas@pc-sistemas-ases.mshome.net>
2022-08-03 15:55:46 -0500

    actualización nodos

--- nodo5.sh
+++ nodo5.sh
@@ -1,3 +1,8 @@
 dato=$1
-filtro=$(cut -d ‘,’ -f 1, 4, 5, 9, 15 $dato)
+filtro=$(cut -d ‘,’ -f 1,15,9 $dato)
+cantidad1=$(grep "Black" $dato | grep ">50k" | wc -l)
+cantidad2=$(grep "Female" $dato | grep ">50k" | wc -l)
 
+echo $filtro
+echo "Numero de personas Black que tienen un income superior a 50K: ${cantidad1}"
+echo "Numero de personas Female que tienen un income superior a 50K: ${cantidad2}"
