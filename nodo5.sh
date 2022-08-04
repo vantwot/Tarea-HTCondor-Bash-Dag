20a594433994469ef5254b458a0957fb3d7e2073
Invitado <invitado@s2pc33.eisc.univalle.edu.co>
2022-08-03 19:49:46 -0500

    actualizacion

--- nodo5.sh
+++ nodo5.sh
@@ -1,7 +1,7 @@
 dato=$1
 filtro=$(cut -d, -f 1,15,9 $dato)
-cantidad1=$(grep "Black" $dato | grep ">50k" | wc -l)
-cantidad2=$(grep "Female" $dato | grep ">50k" | wc -l)
+cantidad1=$(grep ">50K" $dato | grep "Black" | wc -l)
+cantidad2=$(grep ">50K" $dato | grep "Female" | wc -l)
 
 echo $filtro
 echo "Numero de personas Black que tienen un income superior a 50K: ${cantidad1}"
