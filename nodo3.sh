c4f0a0963a008a08a7e8112cfc61403def1c1116
Van <45314803+vantwot@users.noreply.github.com>
2022-08-03 22:11:18 -0500

    correciones #2

--- nodo3.sh
+++ nodo3.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/bin/bash
 
 dato=$1
-encabezado=$(sed -i 1i "age,type_employer, fnlwgt, education, education_num, marital, occupation, relationship, race, sex, capital_gain, capital_loss, hr_per_week, country, income" $dato)
+encabezado=$(sed -i 1i"age,type_employer, fnlwgt, education, education_num, marital, occupation, relationship, race, sex, capital_gain, capital_loss, hr_per_week, country, income" $dato)
